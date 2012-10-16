6a70cd3379630c5dadd2902f3d32cce315c1bf95
Marc Fournier <marc.fournier@camptocamp.com>
2012-10-16 07:36:52 +0200

    augeas: use incl/lens to make loading faster.

--- manifests/virtual.pp
+++ manifests/virtual.pp
@@ -59,7 +59,8 @@ define postfix::virtual (
 
   augeas {"Postfix virtual - ${name}":
     load_path => '/usr/share/augeas/lenses/contrib/',
-    context   => "/files${file}",
+    incl      => $file,
+    lens      => 'Postfix_Virtual.aug',
     changes   => $changes,
     require   => [Package['postfix'], Augeas::Lens['postfix_transport']],
     notify    => Exec['generate /etc/postfix/virtual.db'],
