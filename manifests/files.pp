a399f4b49a812db8226d1b130e5984287723a9e0
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-05-20 13:57:19 +0200

    Split init.pp into packages/files/service and add parameters

---
+++ manifests/files.pp
@@ -0,0 +1,71 @@
+class postfix::files {
+  include postfix::params
+
+  file { '/etc/mailname':
+    ensure  => present,
+    content => "${::fqdn}\n",
+    seltype => $postfix::params::seltype,
+  }
+
+  # Aliases
+  file { '/etc/aliases':
+    ensure  => present,
+    content => '# file managed by puppet\n',
+    replace => false,
+    seltype => $postfix::params::seltype,
+    notify  => Exec['newaliases'],
+  }
+
+  # Aliases
+  exec { 'newaliases':
+    command     => '/usr/bin/newaliases',
+    refreshonly => true,
+    subscribe   => File['/etc/aliases'],
+  }
+
+  # Config files
+  file { '/etc/postfix/master.cf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template(
+      $postfix::params::master_os_template,
+      "${module_name}/master.cf.common.erb"
+    ),
+    seltype => $postfix::params::seltype,
+  }
+
+  # Config files
+  file { '/etc/postfix/main.cf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => "puppet:///modules/${module_name}/main.cf",
+    replace => false,
+    seltype => $postfix::params::seltype,
+  }
+
+  ::postfix::config {
+    'myorigin':         value => $postfix::myorigin;
+    'alias_maps':       value => 'hash:/etc/aliases';
+    'inet_interfaces':  value => $postfix::inet_interfaces;
+  }
+
+  case $::osfamily {
+    'RedHat': {
+      ::postfix::config {
+        'sendmail_path':    value => '/usr/sbin/sendmail.postfix';
+        'newaliases_path':  value => '/usr/bin/newaliases.postfix';
+        'mailq_path':       value => '/usr/bin/mailq.postfix';
+      }
+    }
+    default: {}
+  }
+
+  mailalias {'root':
+    recipient => $postfix::root_mail_recipient,
+    notify    => Exec['newaliases'],
+  }
+}
