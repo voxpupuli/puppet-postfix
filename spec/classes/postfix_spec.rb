fc90073c5ab7e14a8fa244f591966791b1bd8ff0
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-05-20 14:23:07 +0200

    Fix quotes in /etc/aliases

--- spec/classes/postfix_spec.rb
+++ spec/classes/postfix_spec.rb
@@ -5,17 +5,25 @@ describe 'postfix' do
     let (:facts) { {
       :operatingsystem => 'Debian',
       :osfamily        => 'Debian',
+      :fqdn            => 'fqdn.example.com',
     } }
 
     it { should contain_package('postfix') }
+    it { should contain_package('mailx') }
+    it { should contain_file('/etc/mailname').with_content("fqdn.example.com\n") }
+    it { should contain_file('/etc/aliases').with_content("# file managed by puppet\n") }
   end
 
   context 'when on RedHat' do
     let (:facts) { {
       :operatingsystem => 'RedHat',
       :osfamily        => 'RedHat',
+      :fqdn            => 'fqdn.example.com',
     } }
 
     it { should contain_package('postfix') }
+    it { should contain_package('mailx') }
+    it { should contain_file('/etc/mailname').with_content("fqdn.example.com\n") }
+    it { should contain_file('/etc/aliases').with_content("# file managed by puppet\n") }
   end
 end
