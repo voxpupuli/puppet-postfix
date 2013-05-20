22437173eb23c5431005dcac6b45d559879cf22c
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-05-20 14:24:56 +0200

    Add tests

--- spec/classes/postfix_spec.rb
+++ spec/classes/postfix_spec.rb
@@ -10,8 +10,11 @@ describe 'postfix' do
 
     it { should contain_package('postfix') }
     it { should contain_package('mailx') }
+
     it { should contain_file('/etc/mailname').with_content("fqdn.example.com\n") }
     it { should contain_file('/etc/aliases').with_content("# file managed by puppet\n") }
+    it { should contain_exec('newaliases').with_refreshonly('true') }
+    it { should contain_file('/etc/postfix/master.cf') }
   end
 
   context 'when on RedHat' do
@@ -23,7 +26,10 @@ describe 'postfix' do
 
     it { should contain_package('postfix') }
     it { should contain_package('mailx') }
+
     it { should contain_file('/etc/mailname').with_content("fqdn.example.com\n") }
     it { should contain_file('/etc/aliases').with_content("# file managed by puppet\n") }
+    it { should contain_exec('newaliases').with_refreshonly('true') }
+    it { should contain_file('/etc/postfix/master.cf') }
   end
 end
