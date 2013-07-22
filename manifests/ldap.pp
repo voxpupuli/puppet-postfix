9ae0499db7667852ca0368854948d0da790db851
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-07-22 16:56:21 +0200

    Add missing variables for ldap

--- manifests/ldap.pp
+++ manifests/ldap.pp
@@ -6,6 +6,16 @@ class postfix::ldap {
     fail 'Missing $postfix_ldap_base !'
   }
 
+  $ldap_host = $postfix::ldap_host ? {
+    undef   => 'localhost',
+    default => $postfix::ldap_host,
+  }
+  $ldap_base = $postfix::ldap_base
+  $ldap_options = $postfix::ldap_options ? {
+    undef   => '',
+    default => $postfix::ldap_options,
+  }
+
   file {'/etc/postfix/ldap-aliases.cf':
     ensure  => present,
     owner   => root,
