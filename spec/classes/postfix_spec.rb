325dc17d5ed5ee630e477c3c2ec05586adff5c05
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-11-17 15:58:50 +0100

    Fix unit tests for postfix class

--- spec/classes/postfix_spec.rb
+++ spec/classes/postfix_spec.rb
@@ -47,11 +47,11 @@ describe 'postfix' do
       it { should contain_package('postfix') }
       it { should contain_package('mailx') }
 
-      it { should contain_file('/etc/mailname').without('seltype').with_content("fqdn.example.com\n") }
-      it { should contain_file('/etc/aliases').without('seltype').with_content("# file managed by puppet\n") }
+      it { should contain_file('/etc/mailname').with_seltype('postfix_etc_t').with_content("fqdn.example.com\n") }
+      it { should contain_file('/etc/aliases').with_seltype('postfix_etc_t').with_content("# file managed by puppet\n") }
       it { should contain_exec('newaliases').with_refreshonly('true') }
-      it { should contain_file('/etc/postfix/master.cf').without('seltype') }
-      it { should contain_file('/etc/postfix/main.cf').without('seltype') }
+      it { should contain_file('/etc/postfix/master.cf').with_seltype('postfix_etc_t') }
+      it { should contain_file('/etc/postfix/main.cf').with_seltype('postfix_etc_t') }
 
       it { should contain_postfix__config('myorigin').with_value('fqdn.example.com') }
       it { should contain_postfix__config('alias_maps').with_value('hash:/etc/aliases') }
