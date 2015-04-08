5b91629ba62c3036e9c27f12e268a03e9300b208
Craig Watson <craig@cwatson.org>
2015-04-08 20:46:31 +0100

    Fixing Augeas deprecation warnings

--- manifests/augeas.pp
+++ manifests/augeas.pp
@@ -3,20 +3,20 @@
 #
 class postfix::augeas {
   augeas::lens {'postfix_transport':
-    ensure      => present,
-    lens_source => 'puppet:///modules/postfix/lenses/postfix_transport.aug',
-    test_source => 'puppet:///modules/postfix/lenses/test_postfix_transport.aug',
-    stock_since => '1.0.0',
+    ensure       => present,
+    lens_content => template('postfix/lenses/postfix_transport.aug'),
+    test_content => template('postfix/lenses/test_postfix_transport.aug'),
+    stock_since  => '1.0.0',
   }
   augeas::lens {'postfix_virtual':
-    ensure      => present,
-    lens_source => 'puppet:///modules/postfix/lenses/postfix_virtual.aug',
-    test_source => 'puppet:///modules/postfix/lenses/test_postfix_virtual.aug',
-    stock_since => '1.0.0',
+    ensure       => present,
+    lens_content => template('postfix/postfix_virtual.aug'),
+    test_content => template('postfix/test_postfix_virtual.aug'),
+    stock_since  => '1.0.0',
   }
   augeas::lens {'postfix_canonical':
-    ensure      => present,
-    lens_source => 'puppet:///modules/postfix/lenses/postfix_canonical.aug',
-    test_source => 'puppet:///modules/postfix/lenses/test_postfix_canonical.aug',
+    ensure       => present,
+    lens_content => template('postfix/postfix_canonical.aug'),
+    test_content => template('postfix/test_postfix_canonical.aug'),
   }
 }
