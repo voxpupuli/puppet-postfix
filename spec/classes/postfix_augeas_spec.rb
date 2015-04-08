d35a9eb857be19dfb350a563a404312542edfe1f
Craig Watson <craig@cwatson.org>
2015-04-08 20:55:45 +0100

    Fixing tests

--- spec/classes/postfix_augeas_spec.rb
+++ spec/classes/postfix_augeas_spec.rb
@@ -17,16 +17,16 @@ describe 'postfix::augeas' do
       it { is_expected.to compile.with_all_deps }
 
       it { is_expected.to contain_augeas__lens('postfix_transport').with({
-        :ensure      => 'present',
-        :lens_source => 'puppet:///modules/postfix/lenses/postfix_transport.aug',
-        :test_source => 'puppet:///modules/postfix/lenses/test_postfix_transport.aug',
-        :stock_since => '1.0.0',
+        :ensure       => 'present',
+        :lens_content => template('postfix/lenses/postfix_transport.aug'),
+        :test_content => template('postfix/lenses/test_postfix_transport.aug'),
+        :stock_since  => '1.0.0',
       } ) }
       it { is_expected.to contain_augeas__lens('postfix_virtual').with({
-        :ensure      => 'present',
-        :lens_source => 'puppet:///modules/postfix/lenses/postfix_virtual.aug',
-        :test_source => 'puppet:///modules/postfix/lenses/test_postfix_virtual.aug',
-        :stock_since => '1.0.0',
+        :ensure       => 'present',
+        :lens_content => template('postfix/lenses/postfix_virtual.aug'),
+        :test_content => template('postfix/lenses/test_postfix_virtual.aug'),
+        :stock_since  => '1.0.0',
       }) }
     end
   end
