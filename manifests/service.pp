503235889db4ebf8f8a737e8b312e1a13d3b37e0
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-06-22 09:07:25 +0200

    Restart postfix instead of reload after package installation (Fixes #90)

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,4 +1,10 @@
 class postfix::service {
+  exec { 'restart postfix after packages install':
+    command     => regsubst($::postfix::params::restart_cmd, 'reload', 'restart'),
+    refreshonly => true,
+    subscribe   => Package['postfix'],
+    require     => Class['postfix::files'],
+  }
   service { 'postfix':
     ensure    => running,
     enable    => true,
