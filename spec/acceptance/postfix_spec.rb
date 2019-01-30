346b3159114f6925c5cd3adce7b9aba75677dff2
your1p <youri.polydore@adullact.org>
2019-01-30 16:44:16 +0100

    add the possibility to manage (or not) aliases

--- spec/acceptance/postfix_spec.rb
+++ spec/acceptance/postfix_spec.rb
@@ -40,4 +40,105 @@ describe 'postfix class' do
       it { is_expected.to be_running }
     end
   end
+
+  context 'default parameters' do
+    it 'should work idempotently with no errors and with the default configuration of /etc/aliases ' do
+      pp = <<-EOS
+        # Make sure the default mailer is stopped in docker containers
+        if $::operatingsystem == 'Debian' {
+          service { 'exim4':
+            ensure    => stopped,
+            hasstatus => false,
+            before    => Class['postfix'],
+          }
+        }
+        if $::osfamily == 'RedHat' {
+          service { 'sendmail':
+            ensure    => stopped,
+            hasstatus => false,
+            before    => Class['postfix'],
+          }
+        }
+
+        class { 'postfix':
+          smtp_listen => 'all',
+        }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes  => true)
+    end
+  end
+
+  context 'default parameters with manage_aliase as false' do
+    it 'should work idempotently with no errors and with your own configuration of /etc/aliases ' do
+      pp = <<-EOS
+        # Make sure the default mailer is stopped in docker containers
+        if $::operatingsystem == 'Debian' {
+          service { 'exim4':
+            ensure    => stopped,
+            hasstatus => false,
+            before    => Class['postfix'],
+          }
+        }
+        if $::osfamily == 'RedHat' {
+          service { 'sendmail':
+            ensure    => stopped,
+            hasstatus => false,
+            before    => Class['postfix'],
+          }
+        }
+
+        class { 'postfix':
+          smtp_listen    => 'all',
+          manage_aliases => false,
+        }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes  => true)
+    end
+
+    describe file('/etc/aliases') do
+      it { is_expected.not_to exist }
+    end
+  end
+
+  context 'default parameters with manage_aliases as undef' do
+    it 'should work idempotently with no errors and with your own configuration of /etc/aliases ' do
+      pp = <<-EOS
+        # Make sure the default mailer is stopped in docker containers
+        if $::operatingsystem == 'Debian' {
+          service { 'exim4':
+            ensure    => stopped,
+            hasstatus => false,
+            before    => Class['postfix'],
+          }
+        }
+        if $::osfamily == 'RedHat' {
+          service { 'sendmail':
+            ensure    => stopped,
+            hasstatus => false,
+            before    => Class['postfix'],
+          }
+        }
+
+        class { 'postfix':
+          smtp_listen    => 'all',
+          manage_aliases => undef,
+          aliases_list   => { foo => bar },
+        }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes  => true)
+    end
+
+    describe file('/etc/aliases') do
+      its(:content) { is_expected.to match('foo') }
+    end
+  end
 end
