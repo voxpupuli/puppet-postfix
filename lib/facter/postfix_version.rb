10169ba1e124c01f2a8c289b87d3eac40a19ed6f
Cedric Behlmer <c.behlmer@neusta.de>
2020-01-07 15:07:16 +0100

    Execution change

--- lib/facter/postfix_version.rb
+++ lib/facter/postfix_version.rb
@@ -1,7 +1,7 @@
 if  FileTest.exists?("/etc/postfix/main.cf")
 	Facter.add(:postfix_version) do
 		setcode do
-			%x{postconf mail_version | awk '{print $3}'}
+			Facter::Core::Execution.execute('postconf mail_version').split[4]
 		end
 	end
 end
