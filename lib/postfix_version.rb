cbb068d23185c680aaba0f301dd1f768d8a4ae46
cbehlmer <57090745+cbehlmer@users.noreply.github.com>
2020-01-07 14:57:48 +0100

    Update lib/postfix_version.rb
    Co-Authored-By: Raphaël Pinson <github+aem1eeshi1@raphink.net>

--- lib/postfix_version.rb
+++ lib/postfix_version.rb
@@ -1,5 +1,5 @@
 if  FileTest.exists?("/etc/postfix/main.cf")
-	Facter.add("postfix_version") do
+	Facter.add(:postfix_version) do
 		setcode do
 			%x{postconf mail_version | awk '{print $3}'}
 		end
